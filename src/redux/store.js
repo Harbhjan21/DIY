4badb78b80d4c14d735d65a8eed556191cd21c7d
TusharVyas <96969905+TusharVyas@users.noreply.github.com>
2023-04-21 15:37:42 +0530

    redux dev tools setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware,compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers/rootReducer";
-
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk, logger)));
 
 export default store;
